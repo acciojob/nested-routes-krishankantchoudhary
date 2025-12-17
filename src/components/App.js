5b1bb7c330ba350ef590b067e45f457fad169320
krishan kant choudhary <83417448+krishankantchoudhary@users.noreply.github.com>
2025-12-17 21:03:31 +0530

    q

--- src/components/App.js
+++ src/components/App.js
@@ -6,19 +6,17 @@ import Women from "./Women";
 const App = () => {
   return (
     <BrowserRouter>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/women">Women</Link></li>
+        </ul>
+      </nav>
+
       <main>
         <div>
           <Switch>
-            <Route exact path="/">
-              <nav>
-                <ul>
-                  <li><Link to="/">Home</Link></li>
-                  <li><Link to="/women">Women</Link></li>
-                </ul>
-              </nav>
-              <Home />
-            </Route>
-
+            <Route exact path="/" component={Home} />
             <Route path="/women" component={Women} />
           </Switch>
         </div>
