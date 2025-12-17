c00c91fe3fb2e881cdbb1ae951ba4b66744fbcb4
krishan kant choudhary <83417448+krishankantchoudhary@users.noreply.github.com>
2025-12-17 21:07:26 +0530

    z

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,26 @@
 import React from "react";
-import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
 import Home from "./Home";
 import Women from "./Women";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/women">Women</Link></li>
-        </ul>
-      </nav>
-
       <main>
         <div>
           <Switch>
-            <Route exact path="/" component={Home} />
+            {/* HOME ROUTE */}
+            <Route exact path="/">
+              <nav>
+                <ul>
+                  <li><Link to="/">Home</Link></li>
+                  <li><Link to="/women">Women</Link></li>
+                </ul>
+              </nav>
+              <Home />
+            </Route>
+
+            {/* WOMEN ROUTE */}
             <Route path="/women" component={Women} />
           </Switch>
         </div>
